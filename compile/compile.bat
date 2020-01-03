5d5e9756433e376440c243b7a16a0164ba7edb7d
jimmyfan <fantianming91@gmail.com>
2020-01-03 09:48:07 +0800

    @
    @

--- compile/compile.bat
+++ compile/compile.bat
@@ -1,4 +1,4 @@
-echo off
+@echo off
 d:
 cd D:\programme\jni4net-0.8.8.0-bin\bin
 .\proxygen.exe E:\CWorkspace\PrintHelper\bin\Release\PrintHelper.dll -wd E:\compile -dp E:\CWorkspace\PrintHelper\bin\Release\O2S.Components.PDFRender4NET.dll

52715c55bda3985592af2debc9a2556238e71f18
jimmyfan <fantianming91@gmail.com>
2020-01-03 20:11:05 +0800

    rem
    rem

--- compile/compile.bat
+++ compile/compile.bat
@@ -1,9 +1,9 @@
-::author tianming.fan
+rem author tianming.fan
 @echo off
-::d:
+rem d:
 cd /d D:\programme\jni4net-0.8.8.0-bin\bin
 .\proxygen.exe E:\CWorkspace\PrintHelper\bin\Release\PrintHelper.dll -wd E:\compile -dp E:\CWorkspace\PrintHelper\bin\Release\O2S.Components.PDFRender4NET.dll
-::d:
+rem d:
 cd /d E:\compile
 .\build.cmd
 pause

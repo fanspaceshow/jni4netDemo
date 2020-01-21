cce21e501b33d1a29a1c1b9b9802aef72ce4b936
Timon <fantianming91@gmail.com>
2020-01-21 10:38:28 +0800

    优化
    优化

--- compile/compile.bat
+++ compile/compile.bat
@@ -1,8 +1,8 @@
 @rem author tianming.fan
 @echo off
 @rem d:
-cd /d D:\programme\jni4net-0.8.8.0-bin\bin
-.\proxygen.exe E:\CWorkspace\PrintHelper\bin\Release\PrintHelper.dll -wd E:\compile -dp E:\CWorkspace\PrintHelper\bin\Release\O2S.Components.PDFRender4NET.dll
+@rem cd /d D:\programme\jni4net-0.8.8.0-bin\bin
+D:\programme\jni4net-0.8.8.0-bin\bin\proxygen.exe E:\CWorkspace\PrintHelper\bin\Release\PrintHelper.dll -wd E:\compile -dp E:\CWorkspace\PrintHelper\bin\Release\O2S.Components.PDFRender4NET.dll
 @rem d:
 cd /d E:\compile
 .\build.cmd

e370541b3a79be68077574e4f158f71e30804f44
Timon <fantianming91@gmail.com>
2020-01-22 09:27:34 +0800

    优化
    优化

--- compile/compile.bat
+++ compile/compile.bat
@@ -7,8 +7,8 @@
 D:\programme\jni4net-0.8.8.0-bin\bin\proxygen.exe E:\workspace\CWorkspace\PrintHelper\bin\Release\PrintHelper.dll -wd E:\workspace\compile -dp E:\workspace\CWorkspace\PrintHelper\bin\Release\O2S.Components.PDFRender4NET.dll
 
 
-@rem cd /d E:\workspace\compile
-@rem .\build.cmd
-E:\workspace\compile\build.cmd
+cd /d E:\workspace\compile
+.\build.cmd
+@rem E:\workspace\compile\build.cmd 经过测试必须分开写，不能语句合并到一起
 
 pause
